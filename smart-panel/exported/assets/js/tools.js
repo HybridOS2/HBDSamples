c2433439d28c14f9922f443b2251c9d2f62b2c16
xueshuming <xueshuming@minigui.org>
2023-11-15 16:06:45 +0800

    tune

--- smart-panel/exported/assets/js/tools.js
+++ smart-panel/exported/assets/js/tools.js
@@ -1,7 +1,7 @@
-function load_bundle()
+function init_carousel()
 {
     let elem = document.createElement("script");
-    elem.setAttribute("src", "hvml://localhost/_renderer/_builtin/-/assets/bootstrap-5.3.1-dist/js/bootstrap.bundle.min.js");
+    elem.setAttribute("src", "hvml://localhost/_renderer/_builtin/-/assets/bootstrap-5.3.1-dist/js/bootstrap.min.js");
     elem.setAttribute("type", "text/javascript");
     elem.addEventListener("load", () => {
         const carousel = new bootstrap.Carousel('#id_carousel_slide');
