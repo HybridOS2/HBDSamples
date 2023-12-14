edddcfb9adf9453454dbe1e2972d5ee0642e84bb
xueshuming <xueshuming@minigui.org>
2023-12-14 11:39:41 +0800

    used rdrState:idle to lock screen instead js

--- smart-panel/exported/assets/js/mra_main.js
+++ smart-panel/exported/assets/js/mra_main.js
@@ -33,35 +33,14 @@ function on_any_input(evt) {
 
 var lock_timer;
 var allow_post_music_event = true;
-function start_lockscreen_timer() {
-    console.log("############################################### start lockscreen timer");
+function disable_post_music_event() {
+    console.log("############################################### disable post music event");
     allow_post_music_event = false;
-    const mainContent = document.getElementById('mainContent');
-    lock_count = 0;
-    lock_timer = setInterval(() => {
-        lock_count += 1;
-        let lock_time = parseInt(mainContent.getAttribute('data-time-to-lock'));
-        if (lock_count >= lock_time) {
-            post_hvml_event('locked', mainContent);
-            lock_count = 0;
-        }
-    }, 1000);
-
-    mainContent.addEventListener("mousedown", on_any_input);
-    mainContent.addEventListener("keydown", on_any_input);
-    mainContent.addEventListener("touchstart", on_any_input);
-    mainContent.addEventListener("touchmove", on_any_input);
 }
 
-function stop_lockscreen_timer() {
-    console.log("############################################### stop lockscreen timer");
+function enable_post_music_event() {
+    console.log("############################################### enable post music event");
     allow_post_music_event = true;
-    clearInterval(lock_timer);
-    const mainContent = document.getElementById('mainContent');
-    mainContent.removeEventListener("mousedown", on_any_input);
-    mainContent.removeEventListener("keydown", on_any_input);
-    mainContent.removeEventListener("touchstart", on_any_input);
-    mainContent.removeEventListener("touchmove", on_any_input);
 }
 
 function post_music_player_event(data) {
